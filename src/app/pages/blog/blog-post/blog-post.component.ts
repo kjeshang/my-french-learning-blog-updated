86594e4fa7a7ecba805bd53bf42e3148a0de19c0
kjeshang <kunal.jeshang@gmail.com>
2025-05-25 15:39:29 -0700

    Incorporated blog container

--- src/app/pages/blog/blog-post/blog-post.component.ts
+++ src/app/pages/blog/blog-post/blog-post.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { BlogStore } from '../../../state/blog.store';
 import { CommonModule } from '@angular/common';
 import { MarkdownModule } from 'ngx-markdown';
+import { BlogContainerComponent } from '../../../shared/page-components/blog-container/blog-container.component';
 
 @Component({
   selector: 'app-blog-post',
-  imports: [CommonModule, MarkdownModule],
+  imports: [CommonModule, MarkdownModule, BlogContainerComponent],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.scss'
 })
---
+++ src/app/shared/page-components/blog-container/blog-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-container',
+  imports: [],
+  templateUrl: './blog-container.component.html',
+  styleUrl: './blog-container.component.scss'
+})
+export class BlogContainerComponent {
+
+}
