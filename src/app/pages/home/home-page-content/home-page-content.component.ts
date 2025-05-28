be651cd9cb3e4b3a71e7dca19a65af60d7fd61ec
kjeshang <kunal.jeshang@gmail.com>
2025-05-27 20:40:10 -0700

    created new component to house main content for home page.

---
+++ src/app/pages/home/home-page-content/home-page-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-content',
+  imports: [],
+  templateUrl: './home-page-content.component.html',
+  styleUrl: './home-page-content.component.scss'
+})
+export class HomePageContentComponent {
+
+}
--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 import { CommonModule } from '@angular/common';
+import { HomePageContentComponent } from '../home-page-content/home-page-content.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [FeatureContainerComponent, CommonModule],
+  imports: [FeatureContainerComponent, CommonModule, HomePageContentComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
