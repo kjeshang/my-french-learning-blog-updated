067f7d2ec8af170b42786cb149140c7a58de0679
kjeshang <kunal@zypgallery.ca>
2025-05-30 09:26:45 -0700

    Created data page component.

---
+++ src/app/pages/data/data-page/data-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-page',
+  imports: [],
+  templateUrl: './data-page.component.html',
+  styleUrl: './data-page.component.scss'
+})
+export class DataPageComponent {
+
+}
