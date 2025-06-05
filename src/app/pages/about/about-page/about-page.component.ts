4bc7aa96f25fbaa817dcfa242e196064ad9eed77
kjeshang <kunal@zypgallery.ca>
2025-06-04 19:23:31 -0700

    started to work on introduction

--- src/app/pages/about/about-page/about-page.component.ts
+++ src/app/pages/about/about-page/about-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
+import { IntroductionComponent } from '../introduction/introduction.component';
 
 @Component({
   selector: 'app-about-page',
-  imports: [FeatureContainerComponent],
+  imports: [FeatureContainerComponent, IntroductionComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
