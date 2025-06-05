19ea40c2906f1d08ad176e6c2631e320d2ab1f1e
kjeshang <kunal@zypgallery.ca>
2025-06-04 19:56:34 -0700

    Created education component

--- src/app/pages/about/about-page/about-page.component.ts
+++ src/app/pages/about/about-page/about-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 import { IntroductionComponent } from '../introduction/introduction.component';
+import { EducationComponent } from '../education/education.component';
 
 @Component({
   selector: 'app-about-page',
-  imports: [FeatureContainerComponent, IntroductionComponent],
+  imports: [FeatureContainerComponent, IntroductionComponent, EducationComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
