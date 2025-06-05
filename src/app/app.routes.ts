b891e91c82a63129c61bafcf4f677c5f1cc15100
kjeshang <kunal@zypgallery.ca>
2025-06-04 18:31:59 -0700

    generated about page component and added to app routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './pages/home/home-page/home-page.component';
 import { BlogPageComponent } from './pages/blog/blog-page/blog-page.component';
 import { NgModule } from '@angular/core';
 import { DataPageComponent } from './pages/data/data-page/data-page.component';
+import { AboutPageComponent } from './pages/about/about-page/about-page.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,9 @@ export const routes: Routes = [
     },
     {
         path:"blog", component:BlogPageComponent
+    },
+    {
+        path:"about", component:AboutPageComponent
     },
     {
         path:"data", component:DataPageComponent
---
+++ src/app/pages/about/about-page/about-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
+
+@Component({
+  selector: 'app-about-page',
+  imports: [FeatureContainerComponent],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.scss'
+})
+export class AboutPageComponent {
+
+}
