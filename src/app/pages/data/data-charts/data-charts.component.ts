f6470c8ff14c38abefb3216fcd99cda93510112b
kjeshang <kunal@zypgallery.ca>
2025-06-04 18:26:55 -0700

    Added a description to the data page

--- src/app/pages/data/data-charts/data-charts.component.ts
+++ src/app/pages/data/data-charts/data-charts.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PlotlyBarChartComponent } from '../../../shared/ui-components/plotly-bar-chart/plotly-bar-chart.component';
+import { PlotlyPieChartComponent } from '../../../shared/ui-components/plotly-pie-chart/plotly-pie-chart.component';
+import { PlotlyLineChartComponent } from '../../../shared/ui-components/plotly-line-chart/plotly-line-chart.component';
+import { BlogStore } from '../../../state/blog.store';
 
 @Component({
   selector: 'app-data-charts',
-  imports: [],
+  imports: [PlotlyBarChartComponent, PlotlyPieChartComponent, PlotlyLineChartComponent],
   templateUrl: './data-charts.component.html',
   styleUrl: './data-charts.component.scss'
 })
 export class DataChartsComponent {
-
+  blogStore = inject(BlogStore);
 }
--- src/app/pages/data/data-page/data-page.component.ts
+++ src/app/pages/data/data-page/data-page.component.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { PlotlyBarChartComponent } from '../../../shared/ui-components/plotly-bar-chart/plotly-bar-chart.component';
 import { FeatureContainerComponent } from "../../../shared/page-components/feature-container/feature-container.component";
 import { BlogStore } from '../../../state/blog.store';
-import { PlotlyPieChartComponent } from '../../../shared/ui-components/plotly-pie-chart/plotly-pie-chart.component';
-import { PlotlyLineChartComponent } from "../../../shared/ui-components/plotly-line-chart/plotly-line-chart.component";
+import { DataChartsComponent } from '../data-charts/data-charts.component';
 
 @Component({
   selector: 'app-data-page',
-  imports: [PlotlyBarChartComponent, FeatureContainerComponent, PlotlyPieChartComponent, PlotlyLineChartComponent],
+  imports: [DataChartsComponent, FeatureContainerComponent],
   templateUrl: './data-page.component.html',
   styleUrl: './data-page.component.scss'
 })
