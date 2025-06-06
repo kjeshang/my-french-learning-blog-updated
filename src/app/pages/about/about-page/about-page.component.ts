f870bc231f125a74d7c8ff0bd3a9455699da3ac9
kjeshang <kunal@zypgallery.ca>
2025-06-05 18:57:43 -0700

    Generated career component

--- src/app/pages/about/about-page/about-page.component.ts
+++ src/app/pages/about/about-page/about-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FeatureContainerComponent } from '../../../shared/page-components/feature-container/feature-container.component';
 import { IntroductionComponent } from '../introduction/introduction.component';
 import { EducationComponent } from '../education/education.component';
+import { CareerComponent } from '../career/career.component';
 
 @Component({
   selector: 'app-about-page',
-  imports: [FeatureContainerComponent, IntroductionComponent, EducationComponent],
+  imports: [FeatureContainerComponent, IntroductionComponent, EducationComponent, CareerComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
---
+++ src/app/pages/about/career/career.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-career',
+  imports: [],
+  templateUrl: './career.component.html',
+  styleUrl: './career.component.scss'
+})
+export class CareerComponent {
+
+}
