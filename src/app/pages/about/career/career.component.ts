db7940288b71fea2ed49e71f9c1f9e132ed303c8
kjeshang <kunal@zypgallery.ca>
2025-06-09 19:40:13 -0700

    Reading career data from object array instead of writing manually in HTML template.

--- src/app/pages/about/career/career.component.ts
+++ src/app/pages/about/career/career.component.ts
@@ -4,8 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-career',
   imports: [],
   templateUrl: './career.component.html',
-  styleUrl: './career.component.scss'
+  styleUrl: './career.component.scss',
 })
 export class CareerComponent {
-
+  careerData: {
+    date: string;
+    position: string;
+    company: string;
+    location: string;
+  }[] = [
+    {
+      date: 'janvier à présent',
+      position: 'Developpeur du logiciel',
+      company: 'A-Mais Technologies Inc. ("Amiga")',
+      location: 'Vancouver du Nord, Colombie-Britannique au Canada',
+    },
+    {
+      date: 'septembre 2023 à janvier 2025',
+      position: "Chef d'équipe",
+      company: 'Nestlé Nespresso',
+      location: 'Burnaby, Colombie-Britannique au Canada',
+    },
+    {
+      date: 'juin 2021 à september 2023',
+      position: 'Spécialiste du café',
+      company: 'Nestlé Nespresso',
+      location: 'Burnaby, Colombie-Britannique au Canada',
+    },
+  ];
 }
